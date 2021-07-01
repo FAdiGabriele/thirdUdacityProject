39f630f8cc9175f994583a7c12db03f8100ddcb5
Fabio A. di Gabriele <fabioa.digabriele@gmail.com>
2021-07-01 15:51:08 +0200

    working on

--- app/src/main/java/com/udacity/uicomponent/LoadingButton.kt
+++ app/src/main/java/com/udacity/uicomponent/LoadingButton.kt
@@ -7,22 +7,31 @@ import android.graphics.*
 import android.os.Handler
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import com.udacity.R
+import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
 
 
 class LoadingButton @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
     private var textToDisplay = resources.getString(R.string.button_label)
-    private var typedArray = context.obtainStyledAttributes(attrs, R.styleable.LoadingButton, defStyleAttr, 0)
+    private var typedArray = context.obtainStyledAttributes(
+        attrs,
+        R.styleable.LoadingButton,
+        defStyleAttr,
+        0
+    )
+
 
     private var progressValue = typedArray.getInt(R.styleable.LoadingButton_progressValue, 0)
+    private var progressColor = typedArray.getColor(R.styleable.LoadingButton_progessColor, resources.getColor(R.color.colorPrimaryDark, null))
 
     //remember to call invalidate when you want to update the UI after user interact with this view
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Inactive) { p, old, new ->
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Inactive) { p, old, new ->
         when (new) {
             //this is the default state
             ButtonState.Inactive -> {
@@ -57,10 +66,11 @@ class LoadingButton @JvmOverloads constructor(
 
     //Components for drawing the loading button
     private val rect = RectF( //basic shape of rect
-            resources.getDimension(R.dimen.loading_button_left_value),
-            resources.getDimension(R.dimen.loading_button_top_value),
-            0f,
-            resources.getDimension(R.dimen.loading_button_bottom_value))
+        resources.getDimension(R.dimen.loading_button_left_value),
+        resources.getDimension(R.dimen.loading_button_top_value)+ resources.getDimension(R.dimen.loading_button_bottom_value).toInt(),
+        0f,
+        resources.getDimension(R.dimen.loading_button_bottom_value) + resources.getDimension(R.dimen.loading_button_bottom_value).toInt()
+    )
 
     private val rectPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
@@ -76,11 +86,11 @@ class LoadingButton @JvmOverloads constructor(
 
     private val progressRectPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
-        color = resources.getColor(R.color.colorPrimaryDark, null)
+        color = progressColor
     }
 
-//    private val valueAnimator = ValueAnimator()
-
+    private val penguinAnimator = animatePenguin(TimeUnit.SECONDS.toMillis(2))
+    val penguinBitmap = BitmapFactory.decodeResource(context.resources,R.drawable.penguin_icon)
 
 
     override fun onDraw(canvas: Canvas) {
@@ -94,10 +104,10 @@ class LoadingButton @JvmOverloads constructor(
         }
 
         canvas.drawText(
-                textToDisplay,
-                rect.centerX(),
-                rect.centerY() + resources.getDimension(R.dimen.loading_button_top_value)/1.5f,
-                textPaint
+            textToDisplay,
+            rect.centerX(),
+            rect.centerY() + resources.getDimension(R.dimen.loading_button_top_value) / 1.5f,
+            textPaint
         )
     }
 
@@ -106,12 +116,14 @@ class LoadingButton @JvmOverloads constructor(
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
         val h: Int = resolveSizeAndState(
-                MeasureSpec.getSize(w),
-                heightMeasureSpec,
-                0
+            MeasureSpec.getSize(w),
+            heightMeasureSpec,
+            0
         )
         widthSize = deviceWidth
-        heightSize =  resources.getDimension(R.dimen.loading_button_bottom_value).toInt() + resources.getDimension(R.dimen.loading_button_top_value).toInt()
+        heightSize =  resources.getDimension(R.dimen.loading_button_bottom_value).toInt() +
+                resources.getDimension(R.dimen.loading_button_top_value).toInt() +
+                resources.getDimension(R.dimen.loading_button_bottom_value).toInt()
         setMeasuredDimension(widthSize, heightSize)
     }
 
@@ -137,13 +149,14 @@ class LoadingButton @JvmOverloads constructor(
         val progressDimension = (progressValue * rect.right) /100
 
         return RectF(
-                resources.getDimension(R.dimen.loading_button_left_value),
-                resources.getDimension(R.dimen.loading_button_top_value),
-                progressDimension,
-                resources.getDimension(R.dimen.loading_button_bottom_value))
+            resources.getDimension(R.dimen.loading_button_left_value),
+            resources.getDimension(R.dimen.loading_button_top_value),
+            progressDimension,
+            resources.getDimension(R.dimen.loading_button_bottom_value)
+        )
     }
 
-    fun setProgressValue(value : Int){
+    fun setProgressValue(value: Int){
         progressValue = value
         invalidate()
 
@@ -155,6 +168,19 @@ class LoadingButton @JvmOverloads constructor(
 
     fun getProgressValue() : Int{
         return progressValue
+    }
+
+    private fun animatePenguin(animationDuration: Long) : ValueAnimator{
+        val anim = ValueAnimator.ofInt(0, 100)
+        anim.duration = animationDuration
+        anim.interpolator = LinearInterpolator()
+        anim.repeatMode = ValueAnimator.RESTART
+        anim.repeatCount = ValueAnimator.INFINITE
+
+        anim.addUpdateListener {
+
+        }
+        return anim
     }
 
 
